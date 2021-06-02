90b1896ba6a96a15b5143cd5571469ccf9afb630
Красимир Беров <berov@cpan.org>
2021-06-02 12:20:05 +0300

    Added dependencies Text::CSV_XS and Imager.
    Initial implementation of the CSV database generation.

--- lib/Mojolicious/Command/Author/generate/obrazi.pm
+++ lib/Mojolicious/Command/Author/generate/obrazi.pm
@@ -1,11 +1,14 @@
 package Mojolicious::Command::Author::generate::obrazi;
-use Mojo::Base 'Mojolicious::Command', -signatures;
+use Mojo::Base Mojolicious::Command => -signatures;
 use Mojo::File 'path';
-use Mojo::Util qw(getopt dumper);
+use Mojo::Util qw(getopt encode decode dumper);
 use Mojo::Collection 'c';
+use Text::CSV_XS qw( csv );
+use Imager;
 
 has description => 'Generate a gallery from a directory structure with images';
 has usage       => sub { shift->extract_usage };
+my $headers = [qw(category path title description author image thumbnail)];
 
 sub run ($self, @args) {
 
@@ -15,8 +18,49 @@ sub run ($self, @args) {
     'x|max=s'    => \(my $max      = '1000x1000'),
     's|thumbs=s' => \(my $thumbs   = '100x100'),
     ;
-  say for (@{path($from_dir)->list_tree});
+  my $matrix = [$headers];
+  my $category;
+  my $root = path($from_dir);
+  $root->list_tree({dir => 1})->sort->each(sub {
+    if (-d $_) {
+      $category = decode('utf-8', $_->to_array->[-1]);
+      push @$matrix, [
+        $category, decode('utf-8', $_->to_string =~ s|$root||r), 'Заглавие на категорията с до две-три думи',
+        'Описание с две до пет изречения на категорията - къде какво, кога, защо, за кого и т.н', 'Марио Беров', '', '',
+
+      ];
+    }
+    else {
+      return if $_ !~ /(?:jpe?g|png|gif)$/i;
+      say $_;
+      my $img;
+      my $warning = '';
+      if (not eval { $img = Imager->new(file => $_) }) {
+        $warning = ' !!! Skipping... ' . Imager->errstr();
+
+        # return;
+      }
+      my $size  = {width => ($img ? $img->getwidth() : 'XXXX'), height => ($img ? $img->getheight() : 'XXXX')};
+      my $image = decode('utf-8', $_->to_array->[-1]);
+      push @$matrix,
+        [
+        $category,
+        decode('utf-8', ($_->to_string =~ s|$root||r) . $warning),
+        'Заглавие на изображението с до две-три думи',
+        'Описание с две до пет изречения на изображението.'
+          . $/
+          . ' Материали, размери,какво, защо - според каквото мислиш, че е важно.',
+        'Марио Беров',
+        $image =~ s/^(.+?)\.(.\w+)$/$1-$size->{width}x$size->{height}.$2/r,
+        '',
+        ];
+    }
+  });
   $self->quiet(0);
+  csv(in => $matrix, enc => "utf-8", out => \my $data, binary => 1, sep_char => ",");
+  path($root, $root->to_array->[-1] . '.csv')->spurt($data);
+
+  # say dumper($matrix);
   return;
 }
 
